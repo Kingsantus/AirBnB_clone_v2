39b91c90524aa8cc4c44dfa716f52fecd537bdcc
kingsantus <kingsantusasogwa@gmail.com>
2023-12-31 12:24:18 -0800

    dynamic change of html

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ This script starts a Flask web application """
 
-from flask import Flask, render_templates
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
