715f173a3cd77cd6aaada751cbd1e352518337d0
kingsantus <kingsantusasogwa@gmail.com>
2023-12-31 23:17:40 -0800

    dynamic html display

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def odd_even(n):
     """ Dislays a HTML page containing even or odd number """
-    return render_template("6-number_odd_or_even.html", number=n)
+    return render_template("6-number_odd_or_even.html", n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
